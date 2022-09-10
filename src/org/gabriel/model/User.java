2b04de5389e2f53a4666eb940e4bcbdc46b40aaf
Gabriel Luciano <gabriel-luciano@outlook.com>
2022-09-10 11:11:53 -0300

    added getter and setter for the name field

--- src/org/gabriel/model/User.java
+++ src/org/gabriel/model/User.java
@@ -3,5 +3,13 @@ package org.gabriel.model;
 public class User {
 
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 }
